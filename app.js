cc7ca61dc2509fee108aac038fa6d231a22c6e43
alexlison <alexlison910@gmail.com>
2025-08-10 11:22:35 +0530

    SignUp Api created

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
+mongoose.connect("mongodb+srv://alexlison:alexlison6885@cluster0.bz3d6.mongodb.net/PawDermaDb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/signup",(req,res) => {
+
+    res.json({"status":"Sucess"})
+
+})
+
 app.listen(4000,() => {
 
     console.log("Server Running at port 4000")
