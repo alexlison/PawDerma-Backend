d9356589f7a968b6212ba05611457b120c530a76
alexlison <alexlison910@gmail.com>
2025-09-12 21:55:02 +0530

    generalBooking Api Updated

--- models/Appointments.js
+++ models/Appointments.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose")
 const appointmentSchema = new mongoose.Schema({
   catOwner_id: { type: mongoose.Schema.Types.ObjectId, ref: "CatOwners", required: true },
   catId: { type: mongoose.Schema.Types.ObjectId, ref: "Cats", required: true },
-  doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "doctors", required: true },
+  scheduleId: { type: mongoose.Schema.Types.ObjectId, ref: "doctorSchedules", required: true }, 
+  token: { type: Number },
+  bookingType: {
+  type: String,
+  enum: ["GENERAL", "SKIN", "VACCINATION"],
+  required: true
+  },
   symptoms: {
     fever: { type: String, enum: ["yes", "no"], default: "no" },
     vomiting: { type: String, enum: ["yes", "no"], default: "no" },
